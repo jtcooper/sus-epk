c8834b34fed7408617fe85a92422bd1c47149b0e
jtcooper <joshcooper537@gmail.com>
2020-01-31 20:51:32 -0700

    added poster for promo vid, which displays first frame when not loaded

--- src/panel/leftPanel.js
+++ src/panel/leftPanel.js
@@ -7,12 +7,15 @@ export class LeftPanel extends React.Component {
       <div>
         <video
           className="youtube-video"
-          controls="controls"
+          controls
           title="promo"
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           src="https://suitupsoldier.com/video/promoJan2020-web.mp4"
-          allowFullScreen></video>
+          poster="img/promoJan2020-paster.jpg"
+          allowFullScreen>
+            Sorry, your browser doesn't support embedded videos.
+        </video>
         <YoutubeFrame title="villains" src="https://www.youtube.com/embed/yvjlmTXLgIg"/>
         <YoutubeFrame title="tongue tied twisted" src="https://www.youtube.com/embed/WEYzM7kCBLg"/>
       </div>
